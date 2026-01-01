f08f8829f0d32f92931f5b3e854f3bf455216e8a
ekrmabd <ekramhossainekram28@gmail.com>
2026-01-01 11:15:51 +0600

    lt 387 && 349

---
+++ leetcode_387.js
@@ -0,0 +1,37 @@
+//387. First Unique Character in a String
+
+var firstUniqChar = function(s) {
+    class HashMap{
+        constructor(){
+            this.map = new Map();
+        }
+        set(key,value){
+            this.map.set(key,value);
+        }
+        get(key){
+            return this.map.has(key)?this.map.get(key):null;
+        }
+    }
+    let map = new Map();
+    let arr = s.split("");
+    for(let i = 0; i < arr.length; i++)
+    {
+        let count = map.get(arr[i]);
+        if(count == null){
+            map.set(arr[i],1);
+        }else{
+            map.set(arr[i],count+1);
+        }
+    }
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(map.get(arr[i]) == 1){
+            return i;
+        }
+    }
+    return -1;
+};
+
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("loveleetcode"));
+console.log(firstUniqChar("aabb"));
