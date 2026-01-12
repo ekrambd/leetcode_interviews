e9c11bf0b5588f14c11c3c3213a22c7cd31fd2c5
ekrmabd <ekramhossainekram28@gmail.com>
2026-01-12 12:30:53 +0600

    leetcode 56 merge intervals

--- leetcode_56.js
+++ leetcode_56.js
@@ -1,15 +1,13 @@
 //56. Merge Intervals
 
 var merge = function(intervals){
-    if(!intervals) return [];
+    if(!intervals.length) return [];
     intervals.sort((a,b)=>a[0]-b[0]);
     let result = [intervals[0]];
-    for(let i = 1; i < intervals.length; i++)
-    {
+    for(let i = 1; i < intervals.length; i++){
         let last = result[result.length - 1];
         let current = intervals[i];
-        if(current[0] <= last[1])
-        {
+        if(current[0] <= last[1]){
             last[1] = Math.max(current[1],last[1]);
         }else{
             result.push(current);
