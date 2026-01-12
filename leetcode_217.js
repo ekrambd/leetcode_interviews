042318f9fda9a782eebda541af288178fd5c61c1
ekrmabd <ekramhossainekram28@gmail.com>
2026-01-12 15:31:42 +0600

    leetcode 217

--- leetcode_217.js
+++ leetcode_217.js
@@ -1,14 +1,11 @@
 //217. Contains Duplicate
 var containsDuplicate = function(nums) {
     let map = new Map();
-    for(let i = 0; i < nums.length; i++)
-    {
-        if(map.has(nums[i])){
-            return true;
-        }
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])) return true;
         map.set(nums[i],i);
     }
-    return false;  
+    return false; 
 };
 console.log(containsDuplicate([1,2,3,1]));
 console.log(containsDuplicate([1,2,3,4]));
